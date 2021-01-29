5906d194664b7ee77b64a0e7d6a87a3695de6f76
Henry Blanchette <henry@monkeyface.com>
2021-01-29 14:00:08 -0500

    renamed isTransitive field of IsEquality

--- src/Relation/Equality.hs
+++ src/Relation/Equality.hs
@@ -13,12 +13,12 @@ data IsEquality eq a = IsEquality
   { eq_isRelation :: IsRelation eq a,
     eq_isReflexive :: IsReflexive eq a,
     eq_isSymmetric :: IsSymmetric eq a,
-    isTransitive :: IsTransitive eq a
+    eq_isTransitive :: IsTransitive eq a
   }
 @-}
 data IsEquality eq a = IsEquality
   { eq_isRelation :: IsRelation eq a,
     eq_isReflexive :: IsReflexive eq a,
     eq_isSymmetric :: IsSymmetric eq a,
-    isTransitive :: IsTransitive eq a
+    eq_isTransitive :: IsTransitive eq a
   }
