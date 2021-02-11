7b2de1d1d0b14f9f8d38da0e481d5e071d9aebc3
Henry Blanchette <henry@monkeyface.com>
2021-02-11 14:44:44 -0500

    fixed notational indentation

--- src/Equality.hs
+++ src/Equality.hs
@@ -20,7 +20,7 @@ An equality is encoded by
 data IsEquality e a <eq :: a -> a -> e a -> Bool> = IsEquality
   { isReflexive :: IsReflexive <eq> e a ,
     isSymmetric :: IsSymmetric <eq> e a ,
-    isTransitive :: IsTransitive <eq> e a 
+    isTransitive :: IsTransitive <eq> e a
   }
 @-}
 data IsEquality e a
@@ -29,17 +29,26 @@ data IsEquality e a
       (IsSymmetric e a)
       (IsTransitive e a)
 
-{-@ isReflexive :: forall <eq :: a -> a -> e a -> Bool>. 
-                   IsEquality <eq> e a -> IsReflexive <eq> e a @-}
+{-@
+isReflexive ::
+  forall <eq :: a -> a -> e a -> Bool>.
+  IsEquality <eq> e a -> IsReflexive <eq> e a
+@-}
 isReflexive :: IsEquality e a -> IsReflexive e a
 isReflexive (IsEquality isReflexive_ _ _) = isReflexive_
 
-{-@ isSymmetric :: forall <eq :: a -> a -> e a -> Bool>. 
-                   IsEquality <eq> e a -> IsSymmetric <eq> e a @-}
+{-@
+isSymmetric ::
+  forall <eq :: a -> a -> e a -> Bool>.
+  IsEquality <eq> e a -> IsSymmetric <eq> e a
+@-}
 isSymmetric :: IsEquality e a -> IsSymmetric e a
 isSymmetric (IsEquality _ isSymmetric_ _) = isSymmetric_
 
-{-@ isTransitive :: forall <eq :: a -> a -> e a -> Bool>. 
-                   IsEquality <eq> e a -> IsTransitive <eq> e a @-}
+{-@
+isTransitive ::
+  forall <eq :: a -> a -> e a -> Bool>.
+  IsEquality <eq> e a -> IsTransitive <eq> e a
+@-}
 isTransitive :: IsEquality e a -> IsTransitive e a
 isTransitive (IsEquality _ _ isTransitive_) = isTransitive_
