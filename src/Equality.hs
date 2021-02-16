727f0c7ccb7fe24dfb46fd74e58ffa04170b8d78
Henry Blanchette <henry@monkeyface.com>
2021-02-16 15:57:46 -0500

    fixed typo from merge

--- src/Equality.hs
+++ src/Equality.hs
@@ -19,7 +19,8 @@ An equality is encoded by
 {-@
 data IsEquality e a <eq :: a -> a -> e a -> Bool> = IsEquality
   { isReflexive :: IsReflexive <eq> e a,
-    isSymmetric :: IsSymmetric <eq> e a 
+    isSymmetric :: IsSymmetric <eq> e a,
+    isTransitive :: IsTransitive <eq> e a
   }
 @-}
 data IsEquality e a
