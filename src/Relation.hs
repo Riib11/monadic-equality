ccc46e38c04e4f00cc48c2d6ad136c9a55ff6d8e
Henry Blanchette <henry@monkeyface.com>
2021-02-16 19:26:25 -0500

    fixed formatting

--- src/Relation.hs
+++ src/Relation.hs
@@ -12,7 +12,7 @@ A relation is encoded by
 
 A witness that `x :: a` and `y :: a` are related has the form
 ```
-  w:(r a)<re x y>
+  w:(r a) <re x y>
 ```
 encoding that `w :: r a` is a witness refined by `re x y`.
 -}
@@ -21,7 +21,7 @@ encoding that `w :: r a` is a witness refined by `re x y`.
 data IsReflexive r a <re :: a -> a -> r a -> Bool> = IsReflexive
   { reflexivity ::
       x:a ->
-      (r a)<re x x>
+      (r a) <re x x>
   }
 @-}
 data IsReflexive r a = IsReflexive (a -> r a)
@@ -33,8 +33,8 @@ reflexivity (IsReflexive reflexivity_) = reflexivity_
 data IsSymmetric r a <re :: a -> a -> r a -> Bool> = IsSymmetric
   { symmetry ::
       x:a -> y:a ->
-      (r a)<re x y> ->
-      (r a)<re y x>
+      (r a) <re x y> ->
+      (r a) <re y x>
   }
 @-}
 data IsSymmetric r a = IsSymmetric (a -> a -> r a -> r a)
@@ -46,12 +46,29 @@ symmetry (IsSymmetric symmetry_) = symmetry_
 data IsTransitive r a <re :: a -> a -> r a -> Bool> = IsTransitive
   { transitivity ::
       x:a -> y:a -> z:a ->
-      (r a)<re x y> ->
-      (r a)<re y z> ->
-      (r a)<re x z>
+      (r a) <re x y> ->
+      (r a) <re y z> ->
+      (r a) <re x z>
   }
 @-}
 data IsTransitive r a = IsTransitive (a -> a -> a -> r a -> r a -> r a)
 
 transitivity :: IsTransitive r a -> (a -> a -> a -> r a -> r a -> r a)
 transitivity (IsTransitive transitivity_) = transitivity_
+
+-- {-@
+-- data PreservesCongruencies r a b <re :: c -> c -> r c -> Bool> =
+--   PreservesCongruencies
+--     { preserves_congruencies ::
+--         x:a -> y:a -> c:(a -> b) ->
+--         (r a) <re x y> ->
+--         (r b) <re (c x) (c y)>
+--     }
+-- @-}
+-- data PreservesCongruencies r a b
+--   = PreservesCongruencies (a -> a -> (a -> b) -> r a -> r b)
+
+-- preserves_congruencies ::
+--   PreservesCongruencies r a b -> a -> a -> (a -> b) -> r a -> r b
+-- preserves_congruencies (PreservesCongruencies preserves_congruencies_) =
+--   preserves_congruencies_
