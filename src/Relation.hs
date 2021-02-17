27a9f3891566c20d001f42fbb43198e16cdca8e0
Henry Blanchette <henry@monkeyface.com>
2021-02-16 20:46:57 -0500

    outlined where IsSubstitutive would go in IsEquality, but having build errors

--- src/Relation.hs
+++ src/Relation.hs
@@ -17,62 +17,62 @@ A witness that `x :: a` and `y :: a` are related has the form
 encoding that `w :: r a` is a witness refined by `re x y`.
 -}
 
--- -- => x ~ x
--- {-@
--- data IsReflexive r a <re :: a -> a -> r a -> Bool> =
---   IsReflexive
---     { reflexivity ::
---         x:a ->
---         (r a) <re x x>
---     }
--- @-}
--- data IsReflexive r a = IsReflexive (a -> r a)
+-- => x ~ x
+{-@
+data IsReflexive r a <re :: a -> a -> r a -> Bool> =
+  IsReflexive
+    { reflexivity ::
+        x:a ->
+        (r a) <re x x>
+    }
+@-}
+data IsReflexive r a = IsReflexive (a -> r a)
 
--- reflexivity :: IsReflexive r a -> (a -> r a)
--- reflexivity (IsReflexive reflexivity_) = reflexivity_
+reflexivity :: IsReflexive r a -> (a -> r a)
+reflexivity (IsReflexive reflexivity_) = reflexivity_
 
--- -- x ~ y  =>  y ~ x
--- {-@
--- data IsSymmetric r a <re :: a -> a -> r a -> Bool> =
---   IsSymmetric
---     { symmetry ::
---         x:a -> y:a ->
---         (r a) <re x y> ->
---         (r a) <re y x>
---     }
--- @-}
--- data IsSymmetric r a = IsSymmetric (a -> a -> r a -> r a)
+-- x ~ y  =>  y ~ x
+{-@
+data IsSymmetric r a <re :: a -> a -> r a -> Bool> =
+  IsSymmetric
+    { symmetry ::
+        x:a -> y:a ->
+        (r a) <re x y> ->
+        (r a) <re y x>
+    }
+@-}
+data IsSymmetric r a = IsSymmetric (a -> a -> r a -> r a)
 
--- symmetry :: IsSymmetric r a -> (a -> a -> r a -> r a)
--- symmetry (IsSymmetric symmetry_) = symmetry_
+symmetry :: IsSymmetric r a -> (a -> a -> r a -> r a)
+symmetry (IsSymmetric symmetry_) = symmetry_
 
--- -- x ~ y, y ~ z  =>  x ~ z
--- {-@
--- data IsTransitive r a <re :: a -> a -> r a -> Bool> =
---   IsTransitive
---     { transitivity ::
---         x:a -> y:a -> z:a ->
---         (r a) <re x y> ->
---         (r a) <re y z> ->
---         (r a) <re x z>
---     }
--- @-}
--- data IsTransitive r a = IsTransitive (a -> a -> a -> r a -> r a -> r a)
+-- x ~ y, y ~ z  =>  x ~ z
+{-@
+data IsTransitive r a <re :: a -> a -> r a -> Bool> =
+  IsTransitive
+    { transitivity ::
+        x:a -> y:a -> z:a ->
+        (r a) <re x y> ->
+        (r a) <re y z> ->
+        (r a) <re x z>
+    }
+@-}
+data IsTransitive r a = IsTransitive (a -> a -> a -> r a -> r a -> r a)
 
--- transitivity :: IsTransitive r a -> (a -> a -> a -> r a -> r a -> r a)
--- transitivity (IsTransitive transitivity_) = transitivity_
+transitivity :: IsTransitive r a -> (a -> a -> a -> r a -> r a -> r a)
+transitivity (IsTransitive transitivity_) = transitivity_
 
--- -- x ~ y  =>  f x ~ f y
--- {-@
--- data IsSubstitutive r a b <re :: c -> c -> r c -> Bool> =
---   IsSubstitutive
---     { substitution ::
---         x:a -> y:a -> c:(a -> b) ->
---         (r a) <re x y> ->
---         (r b) <re (c x) (c y)>
---     }
--- @-}
--- data IsSubstitutive r a b = IsSubstitutive (a -> a -> (a -> b) -> r a -> r b)
+-- x ~ y  =>  f x ~ f y
+{-@
+data IsSubstitutive r a b <re :: forall c. c -> c -> r c -> Bool> =
+  IsSubstitutive
+    { substitution ::
+        x:a -> y:a -> c:(a -> b) ->
+        (r a) <re x y> ->
+        (r b) <re (c x) (c y)>
+    }
+@-}
+data IsSubstitutive r a b = IsSubstitutive (a -> a -> (a -> b) -> r a -> r b)
 
--- substitution :: IsSubstitutive r a b -> a -> a -> (a -> b) -> r a -> r b
--- substitution (IsSubstitutive substitution_) = substitution_
+substitution :: IsSubstitutive r a b -> a -> a -> (a -> b) -> r a -> r b
+substitution (IsSubstitutive substitution_) = substitution_
--- src/Relation/Equality.hs
+++ src/Relation/Equality.hs
@@ -2,63 +2,65 @@ module Relation.Equality where
 
 import Relation
 
--- {-
--- # Equality
+{-
+# Equality
 
--- An equality is encoded by
---   - a witness GADT `e :: * -> *`
---   - an equality measure `eq :: a -> a -> e a -> Bool`
---   - a domain type `a :: *`
---   - proofs that the relation specified by `e`, eq`, `a` is an equivalence
---     relation i.e. is
---       - reflexive (`IsReflexive`)
---       - symmetric (`IsSymmetric`)
---       - transitive (`IsTransitive`)
---       - substitutive (`IsSubstitutive`)
--- -}
+An equality is encoded by
+  - a witness GADT `e :: * -> *`
+  - an equality measure `eq :: a -> a -> e a -> Bool`
+  - a domain type `a :: *`
+  - proofs that the relation specified by `e`, eq`, `a` is an equivalence
+    relation i.e. is
+      - reflexive (`IsReflexive`)
+      - symmetric (`IsSymmetric`)
+      - transitive (`IsTransitive`)
+      - substitutive (`IsSubstitutive`)
+-}
 
--- {-@
--- data IsEquality e a <eq :: a -> a -> e a -> Bool> = IsEquality
---   { isReflexive :: IsReflexive <eq> e a,
---     isSymmetric :: IsSymmetric <eq> e a,
---     isTransitive :: IsTransitive <eq> e a,
---     isSubstitutive :: IsEquality <eq> e b -> IsSubstitutive <eq> e a b
---   }
--- @-}
--- data IsEquality e a
---   = IsEquality
---       (IsReflexive e a)
---       (IsSymmetric e a)
---       (IsTransitive e a)
---       (IsEquality e b -> IsSubstitutive e a b)
+{-@
+data IsEquality e a <eq :: forall a. a -> a -> e a -> Bool> = IsEquality
+  { isReflexive :: IsReflexive <eq> e a,
+    isSymmetric :: IsSymmetric <eq> e a,
+    isTransitive :: IsTransitive <eq> e a
+  }
+@-}
+data IsEquality e a
+  = IsEquality
+      (IsReflexive e a)
+      (IsSymmetric e a)
+      (IsTransitive e a)
 
--- {-@
--- isReflexive ::
---   forall <eq :: a -> a -> e a -> Bool>.
---   IsEquality <eq> e a -> IsReflexive <eq> e a
--- @-}
--- isReflexive :: IsEquality e a -> IsReflexive e a
--- isReflexive (IsEquality isReflexive_ _ _ _) = isReflexive_
+-- isSubstitutive :: forall b. IsEquality <eq> e b -> IsSubstitutive <eq> e a b
+-- (forall b. IsEquality e b -> IsSubstitutive e a b)
 
--- {-@
--- isSymmetric ::
---   forall <eq :: a -> a -> e a -> Bool>.
---   IsEquality <eq> e a -> IsSymmetric <eq> e a
--- @-}
--- isSymmetric :: IsEquality e a -> IsSymmetric e a
--- isSymmetric (IsEquality _ isSymmetric_ _ _) = isSymmetric_
+{-@
+isReflexive ::
+  forall <eq :: a -> a -> e a -> Bool>.
+  IsEquality <eq> e a -> IsReflexive <eq> e a
+@-}
+isReflexive :: IsEquality e a -> IsReflexive e a
+isReflexive (IsEquality isReflexive_ _ _) = isReflexive_
 
--- {-@
--- isTransitive ::
---   forall <eq :: a -> a -> e a -> Bool>.
---   IsEquality <eq> e a -> IsTransitive <eq> e a
--- @-}
--- isTransitive :: IsEquality e a -> IsTransitive e a
--- isTransitive (IsEquality _ _ isTransitive_ _) = isTransitive_
+{-@
+isSymmetric ::
+  forall <eq :: a -> a -> e a -> Bool>.
+  IsEquality <eq> e a -> IsSymmetric <eq> e a
+@-}
+isSymmetric :: IsEquality e a -> IsSymmetric e a
+isSymmetric (IsEquality _ isSymmetric_ _) = isSymmetric_
+
+{-@
+isTransitive ::
+  forall <eq :: a -> a -> e a -> Bool>.
+  IsEquality <eq> e a -> IsTransitive <eq> e a
+@-}
+isTransitive :: IsEquality e a -> IsTransitive e a
+isTransitive (IsEquality _ _ isTransitive_) = isTransitive_
 
 -- {-@
 -- isSubstitutive ::
---   forall <eq :: a -> a -> e a -> Bool>.
---   IsEquality <eq> e a -> IsEquality <eq> e b -> IsSubstitutive <eq> e a b
+--   forall <eq :: forall a. a -> a -> e a -> Bool>.
+--   IsEquality <eq> e a -> forall b. IsEquality <eq> e b -> IsSubstitutive <eq> e a b
 -- @-}
+-- isSubstitutive :: IsEquality e a -> forall b. IsEquality e b -> IsSubstitutive e a b
 -- isSubstitutive (IsEquality _ _ _ isSubstitutive_) = isSubstitutive_
