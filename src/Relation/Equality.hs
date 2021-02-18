5320d706e883735f14908c6ccf39f31a7aac6b21
Henry Blanchette <henry@monkeyface.com>
2021-02-17 20:33:37 -0500

    added documentation for extraparameterized IsEquaity

--- src/Relation/Equality.hs
+++ src/Relation/Equality.hs
@@ -7,7 +7,7 @@ import Relation
 
 An equality is encoded by
   - a witness GADT `e :: * -> *`
-  - an equality measure `eq :: a -> a -> e a -> Bool`
+  - an equality measure `eq :: a -> a -> e a -> Bool` (and a copy `eqb`, since abstract predicates are not polymorphic)
   - a domain type `a :: *`
   - proofs that the relation specified by `e`, eq`, `a` is an equivalence
     relation i.e. is
