38379efff4f1f5af576c2d652021aaee8232b20d
Henry Blanchette <henry@monkeyface.com>
2021-02-22 13:53:56 -0500

    fixed ReflexivityEqualityProp (a -> b) error

--- src/Relation/Equality/Prop.hs
+++ src/Relation/Equality/Prop.hs
@@ -18,12 +18,12 @@ type EqualProp a X Y = {w:EqualityProp a | eqprop X Y w}
 {-@
 data EqualityProp :: * -> * where
     FromSMT :: x:a -> y:a -> EqualSMT a {x} {y} -> EqualProp a {x} {y}
-  | Extensionality :: f:(a -> b) -> g:(a -> b) -> (x:a -> EqualProp a {f x} {g x}) -> EqualProp (a -> b) {f} {g}
+  | Extensionality :: f:(a -> b) -> g:(a -> b) -> (x:a -> EqualProp b {f x} {g x}) -> EqualProp (a -> b) {f} {g}
   | SubstitutitivityProp :: x:a -> y:a -> c:(a -> b) -> EqualProp a {x} {y} -> EqualProp b {c x} {c y}
 @-}
 data EqualityProp :: * -> * where
   FromSMT :: a -> a -> EqualitySMT a -> EqualityProp a
-  Extensionality :: (a -> b) -> (a -> b) -> (a -> EqualityProp a) -> EqualityProp (a -> b)
+  Extensionality :: (a -> b) -> (a -> b) -> (a -> EqualityProp b) -> EqualityProp (a -> b)
   SubstitutitivityProp :: a -> a -> (a -> b) -> EqualityProp a -> EqualityProp b
 
 {-@
@@ -69,34 +69,6 @@ class ReflexivityEqualityProp a where
 
 instance EqSMT a => ReflexivityEqualityProp a where
   reflexivityEqualityProp x = FromSMT x x (FromPrim x x trivial)
-
-{-
-TODO: causes error
-
-    • Couldn't match type ‘b’ with ‘a’
-      ‘b’ is a rigid type variable bound by
-        the instance declaration
-        at src/Relation/Equality/Prop.hs:73:10-70
-      ‘a’ is a rigid type variable bound by
-        the instance declaration
-        at src/Relation/Equality/Prop.hs:73:10-70
-      Expected type: EqualityProp a
-        Actual type: EqualityProp b
-    • In the expression: reflexivityEqualityProp (f x)
-      In the third argument of ‘Extensionality’, namely
-        ‘(\ x -> reflexivityEqualityProp (f x))’
-      In the expression:
-        Extensionality f f (\ x -> reflexivityEqualityProp (f x))
-    • Relevant bindings include
-        x :: a (bound at src/Relation/Equality/Prop.hs:75:26)
-        f :: a -> b (bound at src/Relation/Equality/Prop.hs:74:27)
-        reflexivityEqualityProp :: (a -> b) -> EqualityProp (a -> b)
-          (bound at src/Relation/Equality/Prop.hs:74:3)
-   |
-75 |     Extensionality f f (\x -> reflexivityEqualityProp (f x))
-   |                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
--}
 
 instance ReflexivityEqualityProp b => ReflexivityEqualityProp (a -> b) where
   reflexivityEqualityProp f =
