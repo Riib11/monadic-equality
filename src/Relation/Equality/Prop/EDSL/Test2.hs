39a3fcd317d4df4a77b7bd3ef5782fcabc74928a
Henry Blanchette <henry@monkeyface.com>
2021-04-15 17:01:39 -0400

    [ci skip] added useful doc to extensionality import bug

--- src/Relation/Equality/Prop/EDSL/Test2.hs
+++ src/Relation/Equality/Prop/EDSL/Test2.hs
@@ -43,6 +43,27 @@ test_int =
     (identity (\x -> x))
     (\x -> reflexivity x ? identity (\x -> x) x)
 
+{-
+TODO: throws this error when the definition of propositional equality (as
+commented out above) is imported rather than included in this file (as it would
+be if the code above was not commented.)
+
+/Users/henry/Documents/Projects/monadic-quicksort-verification/monadic-equality/src/Relation/Equality/Prop/EDSL/Test.hs:36:1: error:
+    Liquid Type Mismatch
+    .
+    The inferred type
+      VV : {v : (Relation.Equality.Prop.EqualityProp Relation.Equality.Prop.EDSL.Test.Bit -> Relation.Equality.Prop.EDSL.Test.Bit) | eqprop lq_anf$ lq_anf$
+                                                                                                                                     && v == Relation.Equality.Prop.extensionality lq_anf$ lq_anf$ lq_anf$}
+    .
+    is not a subtype of the required type
+      VV : {VV : (Relation.Equality.Prop.EqualityProp Relation.Equality.Prop.EDSL.Test.Bit -> Relation.Equality.Prop.EDSL.Test.Bit) | eqprop (Function.identity lam x : Bit . x) (Function.identity lam x : Bit . x)}
+    .
+   |
+36 | test_bit =
+   | ^^^^^^^^^^...
+Failed, three modules loaded.
+-}
+
 {-@
 test_bit :: {_:EqualityProp (Bit -> Bit) | eqprop (identity (\x:Bit -> x)) (identity (\x:Bit -> x))}
 @-}
