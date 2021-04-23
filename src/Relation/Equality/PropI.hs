9f2281bf512a610a6a0569e13a0d0c5a82dfe8ed
Henry Blanchette <henry@monkeyface.com>
2021-04-23 15:37:20 -0400

    formatting

--- src/Relation/Equality/PropI.hs
+++ src/Relation/Equality/PropI.hs
@@ -4,6 +4,7 @@ import Function
 import Language.Haskell.Liquid.ProofCombinators
 
 -- a distinct instance for each base type must be posulated
+-- `a` is bivariant!!
 data EqualPropSing a
 
 class EqualPropSingI a where
@@ -18,13 +19,15 @@ type EqualProp S X Y = {_:Proof | eqp S X Y}
 @-}
 
 {-@ assume
-extensionality :: sing_b:EqualPropSing b -> sing_a_to_b:EqualPropSing (a -> b) -> f:(a -> b) -> g:(a -> b) -> (x:a -> EqualProp {sing_b} {f x} {g x}) -> EqualProp {sing_a_to_b} {f} {g}
+extensionality :: sing_b:EqualPropSing b -> sing_a_to_b:EqualPropSing (a -> b) ->
+  f:(a -> b) -> g:(a -> b) -> (x:a -> EqualProp {sing_b} {f x} {g x}) -> EqualProp {sing_a_to_b} {f} {g}
 @-}
 extensionality :: EqualPropSing b -> EqualPropSing (a -> b) -> (a -> b) -> (a -> b) -> (a -> Proof) -> Proof
 extensionality _ _ _ _ _ = ()
 
 {-@
-retractability :: sing_b:EqualPropSing b -> sing_a_to_b:EqualPropSing (a -> b) -> f:(a -> b) -> g:(a -> b) -> EqualProp {sing_a_to_b} {f} {g} -> x:a -> EqualProp {sing_b} {f x} {g x}
+retractability :: sing_b:EqualPropSing b -> sing_a_to_b:EqualPropSing (a -> b) ->
+  f:(a -> b) -> g:(a -> b) -> EqualProp {sing_a_to_b} {f} {g} -> x:a -> EqualProp {sing_b} {f x} {g x}
 @-}
 retractability :: EqualPropSing b -> EqualPropSing (a -> b) -> (a -> b) -> (a -> b) -> Proof -> a -> Proof
 retractability sing_b sing_a_to_b f g efg x =
@@ -46,7 +49,7 @@ Typeclass that combines together the equality properties of propositional equali
 -}
 
 -- reflexivity and substituability are axioms
-type EqP a = (Symmetry a, Transitivity a)
+-- type EqP a = (Symmetry a, Transitivity a)
 
 {-
 ### SMT EqP
@@ -65,7 +68,8 @@ class EqSMT a where
 
 {-@
 class Concreteness a where
-  concreteness :: sing:EqualPropSing a -> x:a -> y:a -> EqualProp {sing} {x} {y} -> {_:Proof | x = y}
+  concreteness :: sing:EqualPropSing a ->
+      x:a -> y:a -> EqualProp {sing} {x} {y} -> {_:Proof | x = y}
 @-}
 class Concreteness a where
   concreteness :: EqualPropSing a -> a -> a -> Proof -> Proof
@@ -74,7 +78,8 @@ instance EqSMT a => Concreteness a where
   concreteness sing x y pf = concreteness_EqSMT sing x y pf
 
 {-@ assume
-concreteness_EqSMT :: EqSMT a => sing:EqualPropSing a -> x:a -> y:a -> EqualProp {sing} {x} {y} -> {_:Proof | x = y}
+concreteness_EqSMT :: EqSMT a => sing:EqualPropSing a ->
+  x:a -> y:a -> EqualProp {sing} {x} {y} -> {_:Proof | x = y}
 @-}
 concreteness_EqSMT :: EqSMT a => EqualPropSing a -> a -> a -> Proof -> Proof
 concreteness_EqSMT _ _ _ _ = ()
@@ -84,7 +89,8 @@ concreteness_EqSMT _ _ _ _ = ()
 -}
 
 {-@ assume
-reflexivity :: sing:EqualPropSing a -> x:a -> EqualProp {sing} {x} {x}
+reflexivity :: sing:EqualPropSing a ->
+  x:a -> EqualProp {sing} {x} {x}
 @-}
 reflexivity :: EqualPropSing a -> a -> Proof
 reflexivity _ _ = ()
@@ -95,7 +101,8 @@ reflexivity _ _ = ()
 
 {-@
 class Symmetry a where
-  symmetry :: sing:EqualPropSing a -> x:a -> y:a -> EqualProp {sing} {x} {y} -> EqualProp {sing} {y} {x}
+  symmetry :: sing:EqualPropSing a ->
+    x:a -> y:a -> EqualProp {sing} {x} {y} -> EqualProp {sing} {y} {x}
 @-}
 class Symmetry a where
   symmetry :: EqualPropSing a -> a -> a -> Proof -> Proof
@@ -119,7 +126,8 @@ instance (Symmetry b, EqualPropSingI b) => Symmetry (a -> b) where
 
 {-@
 class Transitivity a where
-  transitivity :: sing:EqualPropSing a -> x:a -> y:a -> z:a -> EqualProp {sing} {x} {y} -> EqualProp {sing} {y} {z} -> EqualProp  {sing} {x} {z}
+  transitivity :: sing:EqualPropSing a ->
+    x:a -> y:a -> z:a -> EqualProp {sing} {x} {y} -> EqualProp {sing} {y} {z} -> EqualProp  {sing} {x} {z}
 @-}
 class Transitivity a where
   transitivity :: EqualPropSing a -> a -> a -> a -> Proof -> Proof -> Proof
@@ -147,7 +155,8 @@ instance (Transitivity b, EqualPropSingI b) => Transitivity (a -> b) where
 -}
 
 {-@ assume
-substitutability :: sing_a:EqualPropSing a -> sing_b:EqualPropSing b -> f:(a -> b) -> x:a -> y:a -> EqualProp {sing_a} {x} {y} -> EqualProp {sing_b} {f x} {f y}
+substitutability :: sing_a:EqualPropSing a -> sing_b:EqualPropSing b ->
+  f:(a -> b) -> x:a -> y:a -> EqualProp {sing_a} {x} {y} -> EqualProp {sing_b} {f x} {f y}
 @-}
 substitutability :: EqualPropSing a -> EqualPropSing b -> (a -> b) -> a -> a -> Proof -> Proof
 substitutability _ _ _ _ _ _ = ()
@@ -157,13 +166,12 @@ substitutability _ _ _ _ _ _ = ()
 -}
 
 {-@
-alphaEquivalency :: (EqualPropSingI b) => sing:EqualPropSing (a -> b) -> f:(a -> b) -> EqualProp {sing} {f} {apply (\x:a -> f x)}
+alphaEquivalency :: forall a b. EqualPropSingI b => sing_a_to_b:EqualPropSing (a -> b) ->
+  f:(a -> b) -> EqualProp {sing_a_to_b} {f} {apply (\x:a -> f x)}
 @-}
-alphaEquivalency :: (EqualPropSingI b) => EqualPropSing (a -> b) -> (a -> b) -> Proof
+alphaEquivalency :: forall a b. EqualPropSingI b => EqualPropSing (a -> b) -> (a -> b) -> Proof
 alphaEquivalency sing_a_to_b f =
-  extensionality (sing :: EqualPropSing b) sing_a_to_b f (apply (\x -> f x)) $ \y ->
-    reflexivity (sing :: EqualPropSing b) (f y)
-      ? apply (\x -> f x) y
-
--- where
---   sing_b = sing :: EqualPropSing b
+  let sing_b = sing :: EqualPropSing b
+   in extensionality sing_b sing_a_to_b f (apply (\x -> f x)) $ \y ->
+        reflexivity sing_b (f y)
+          ? apply (\x -> f x) y
